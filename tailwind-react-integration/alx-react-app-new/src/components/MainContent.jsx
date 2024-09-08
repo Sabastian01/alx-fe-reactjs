8ac3032aaa1679a4967bbaa96a0bd4e1e2869840
Anim Boateng Sabastian <boatengsabastian92@gmail.com>
2024-09-08 20:11:12 +0000

    walaahi

--- tailwind-react-integration/alx-react-app-new/src/components/MainContent.jsx
+++
@@ -1,9 +0,0 @@
-export default function MainContent() {
-  return (
-    <main>
-      <p style={{ backgroundColor: "wheat", color: "gray" }}>
-        I love to visit New York, Paris, and Tokyo.
-      </p>
-    </main>
-  );
-}
