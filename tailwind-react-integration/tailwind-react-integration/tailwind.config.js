d8d425b179e45179a5f0aa7627e77a8b3fa89532
Anim Boateng Sabastian <boatengsabastian92@gmail.com>
2024-09-08 20:12:49 +0000

    now

--- tailwind-react-integration/tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind-react-integration/tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false,
+  content: [],
   theme: {
     extend: {},
   },
+  plugins: [],
+}
+
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  theme: { extend: {} },
   variants: { extend: {} },
   plugins: [],
 };
